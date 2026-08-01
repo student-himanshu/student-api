6b51eee086e9187ec8878f16f61525fb10563176
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-01 21:15:40 +0530

    Lesson 6 - Added POST API to create student

--- app.js
+++ app.js
@@ -2,100 +2,107 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());//Express ko JSON samajhna nahi aata.// Ye line bolti hai// "Agar koi JSON bheje to usko JavaScript Object me convert kar do."
+
+
 // Home Route
 app.get("/", (req, res) => {
     // res.send("Welcome to Student API");
     res.json({
-    name: "Himanshu",
-    age: 22
-});
-// res.json("hellow world")  ;  
+        name: "Himanshu",
+        age: 22
+    });
+    // res.json("hellow world")  ;  
 
 });
 app.get("/about", (req, res) => {
     res.send("About Page");
 });
-app.get("/content",(req,res)=>{
+app.get("/content", (req, res) => {
     res.send("content page");
 })
 
 
 
-// app.get("/students", (req, res) => {
-
-// res.json(students);
-
-// });
-app.get("/students/:id", (req, res) => {
-    const students = [
-
-    {
-        id:1,
-        name:"Rahul",
-        age:20
-    },
-
-    {
-        id:2,
-        name:"Aman",
-        age:21
-    },
-
-    {
-        id:3,
-        name:"Riya",
-        age:22
-    },
-    {
-        id:4,
-        name:"jay",
-        age:23
-    }
-
-];
-        // console.log(req.params);
-    const id = Number(req.params.id);
-
-    const student = students.find((s) => s.id === id);
-
-    // res.json(student);
-    if(student){
-
-    res.status(200).json(student);
-
-}else{
-
-    res.status(404).json({
-        message:"Student Not Found"
-    });
-
-}
+app.get("/students", (req, res) => {
+    res.json(students);
 
 });
-// app.get("/students/:id", (req, res) => {
+const students = [
 
-//     console.log(req.params.id);
+    {
+        id: 1,
+        name: "Rahul",
+        age: 20
+    },
 
-//     res.send("Student Found");
+    {
+        id: 2,
+        name: "Aman",
+        age: 21
+    },
+    {
+        id: 3,
+        name: "Riya",
+        age: 22
+    },
+    {
+        id: 4,
+        name: "jay",
+        age: 23
+    }
+];
+app.get("/students/:id", (req, res) => {
 
-// });
+    // console.log(req.params);
+    const id = Number(req.params.id);
+    const student = students.find((s) => s.id === id);
+    // res.json(student);
+    if (student) {
+        res.status(200).json(student);
+    } else {
+        res.status(404).json({
+            message: "Student Not Found"
+        });
+    }
+});
+
+// postman se post jayega naki browser se body=>row=>json select=>{name:"hin",age:22}
+
+app.post("/students", (req, res) => {
+
+    const newStudent = {
+
+        id: students.length + 1,
+
+        name: req.body.name,
+
+        age: req.body.age
+
+    };
+
+    students.push(newStudent);
+    res.status(201).json(newStudent);
+
+});
+
 app.get("/teachers", (req, res) => {
     res.send("All Teachers");
 });
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.send("login page");
 
 })
-app.get("/signup",(req,res)=>{
+app.get("/signup", (req, res) => {
     res.send("signup");
 })
-app.get("/profile",(req,res)=>{
+app.get("/profile", (req, res) => {
     res.send("profile");
 })
-app.get("/courses",(req,res)=>{
+app.get("/courses", (req, res) => {
     res.send("courses");
 })
-app.get("/results",(req,res)=>{
+app.get("/results", (req, res) => {
     res.send("results");
 })
 app.get("/", (req, res) => {
