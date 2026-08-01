82bab35033667b96851ae1c9fd6e90a9e0207a81
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-01 13:01:39 +0530

    Added nodemon setup

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Welcome to Student API");
 });
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+app.get("/content",(req,res)=>{
+    res.send("content page");
+})
+app.get("/students", (req, res) => {
+    res.send("All Students");
+});
+app.get("/teachers", (req, res) => {
+    res.send("All Teachers");
+});
 
 app.listen(3000, () => {
     console.log("Server Started");
