b5595320b2f51574b97fba9a55f913333b81ddd7
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-03 02:29:48 +0530

    Lesson 14 Part 3 - Added Login API with JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 
 const signup = async (req, res) => {
 
@@ -60,9 +63,97 @@ const signup = async (req, res) => {
     }
 
 };
+const login = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+
+            return res.status(400).json({
+
+                message: "Email and Password required"
+
+            });
+
+        }
+
+        const user = await User.findOne({
+
+            email
+
+        });
+
+        if (!user) {
+
+            return res.status(404).json({
+
+                message: "User Not Found"
+
+            });
+
+        }
+
+        const isMatch = await bcrypt.compare(
+
+            password,
+
+            user.password
+
+        );
+
+        if (!isMatch) {
+
+            return res.status(401).json({
+
+                message: "Invalid Password"
+
+            });
+
+        }
+
+        const token = jwt.sign(
+
+            {
+
+                userId: user._id
+
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+
+                expiresIn: "1d"
+
+            }
+
+        );
+
+        res.status(200).json({
+
+            message: "Login Successful",
+
+            token
+
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+
+            message: error.message
+
+        });
+
+    }
+
+}
 
 module.exports = {
 
-    signup
+    signup,
+    login
 
 };
