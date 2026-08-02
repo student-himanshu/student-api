ef11e34dc74be905db9d1e0055294aea08f9110a
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-03 02:49:28 +0530

    Lesson 14 Part 4 - Added JWT Authentication Middleware

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,14 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
 const validateStudent = require("../middleware/validateStudent");
-const {
-
-    getStudents, getStudentById, addStudent, updateStudent, deleteStudent
-
-} = require("../controllers/studentController");
-
-router.get("/", getStudents);
+const { getStudents, getStudentById, addStudent, updateStudent, deleteStudent } = require("../controllers/studentController");
+const authMiddleware = require("../middleware/authMiddleware");
+router.get("/", authMiddleware, getStudents);
 
 router.get("/:id", getStudentById);
 
